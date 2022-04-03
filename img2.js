4c4786d47cf7839b395f781e6edb4fcf2725807c
mranythingph <65060301+mranythingph@users.noreply.github.com>
2022-04-04 07:30:05 +0800

    Create img2.js

---
+++ img2.js
@@ -0,0 +1,14 @@
+<script>
+function display_image(src, width, height, alt) {
+    var a = document.createElement("img");
+    a.src = src;
+    a.width = width;
+    a.height = height;
+    a.alt = alt;
+    document.body.appendChild(a);
+}
+display_image('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjXqEYdXyzBRlzrGZ55vYiYUbcvPlIm9AqOVJT9uTDx-DcyO8cxja55pixFt1ISEzaUzJf-t8ZFfOTDHx3bIpSFjYHa0UK5VHXrTudHJdhZWrlXCAUNlIOy18sp0bFDY_1w1dM2o4i_xLEKszwxRDYXBwebIoawBSVrHRC1KPDsMZBT8QMN4chL2XoB4A/s320/img2.JPG', 
+                 276, 
+                 110, 
+                 'JavaScriptImage');
+</script>
