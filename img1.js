aa15fca8ff368c6c2876e3119b7adbece6cbb918
mranythingph <65060301+mranythingph@users.noreply.github.com>
2022-04-04 07:27:01 +0800

    Create img1.js

---
+++ img1.js
@@ -0,0 +1,14 @@
+<script>
+function display_image(src, width, height, alt) {
+    var a = document.createElement("img");
+    a.src = src;
+    a.width = width;
+    a.height = height;
+    a.alt = alt;
+    document.body.appendChild(a);
+}
+display_image('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEickU3thOl2f4URJE3Gyf7XTlY1caBo-ym7QnfAyJDn4gpO9c3FpgJ4_jpU1N8ckzD1nLLjOFZcqF18uVv4fZLwUAuQrjwFOnhcFYKiFn0WHDdXiwktMjvVZoj-i67F5VODLUx1N9dlkJgxYTUtFSuOa_gba5hQ14TotwhixRG-rulfBnRE5n4L_ORSxA/s16000/img1.JPG', 
+                 276, 
+                 110, 
+                 'JavaScriptImage');
+</script>
